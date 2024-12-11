6503d364779d1c8796c5c4ced67b1d6539302259
Antonin Severa <antonin.severa.022@pslib.cz>
2024-12-11 12:27:09 +0100

    routeeer

--- gamebook.client/src/App.tsx
+++ gamebook.client/src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css';
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  }
+]);
 
 function App() {
    
