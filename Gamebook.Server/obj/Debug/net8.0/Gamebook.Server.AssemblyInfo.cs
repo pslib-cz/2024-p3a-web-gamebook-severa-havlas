327370dd67f88448d70433a8346038fea1bd3089
Antonin Severa <antonin.severa.022@pslib.cz>
2025-02-26 15:36:58 +0100

    Data

--- Gamebook.Server/obj/Debug/net8.0/Gamebook.Server.AssemblyInfo.cs
+++ Gamebook.Server/obj/Debug/net8.0/Gamebook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e0e6af0441c96e83bc3104857e1e341eab0b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b17eb10c7861f827560f73cf2669f4e9a82b7ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
