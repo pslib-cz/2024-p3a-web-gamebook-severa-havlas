40ab8a1625982a951ec971540f53dfa849b89993
Antonin Severa <antonin.severa.022@pslib.cz>
2025-02-28 09:41:43 +0100

    Postel

--- Gamebook.Server/obj/Debug/net8.0/Gamebook.Server.AssemblyInfo.cs
+++ Gamebook.Server/obj/Debug/net8.0/Gamebook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f00ac7fbe6a330a2e67bdea5c1446266ce4cbc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224ab157653aaac2ec097b1b2b6cea83d00739b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamebook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
